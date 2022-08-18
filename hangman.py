ada4c3cac863161cfd0c3f8d1a5f10277d473593
codingjavi <109295330+codingjavi@users.noreply.github.com>
2022-08-17 19:47:25 -0500

    Update hangman.py
    Now it can find 2 letter and replace them

--- hangman.py
+++ hangman.py
@@ -45,14 +45,31 @@ def check_guess(guess):
 
     #checks all the letters in word
     check = False
-    for i in word:
+    for i in word_board(): #shoes
         if i == guess:
+            #needed help with this
+
+            #enumerate works like this (x, y), x is the number(place) and y(letter) is the value
+            #indices = [0, 4] if shoes is in
+            #i for i in enumerate (i, x) for every x or (letter) in enumerate, AND only goes in list if letter is == guess
+            #letter in enumerate(word) just loops over the letters in enumerate(word) to check if letter == guess
+            #so basically indices only takes the i in (i, x)
+            #enumerate creates an object that contains a counter for each value(letters)
+            #it runs through the object and takes the i if letter == guess
+            #i for (i, letter) in enumerate(word) if letter == guess
+            indices = [i for i, letter in enumerate(word) if letter == guess]
+            #goes thru list of index's in indices
+            for index in indices: #(x, y) for x in indices
+                blank_board[index] = guess
+
+
+            #this was my first attempt
             #finding where in the letter guess equals i using .index()
-            placing = word.index(guess)
-            
+            #check where i is in word_board(could be multiple places)
             #inserting the users guess into the blank board
             #inserting method doesn't work, we have to replace the blank space with the guess
-            blank_board[placing] = guess
+            """placing = word_board().index(i)
+            blank_board[placing] = guess"""
             check = True
     if check == False:
         loop = loop + 1
